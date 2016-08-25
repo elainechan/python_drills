fe6a9ce13844fd4334d89cb87d77ac8d679fe17b
Elaine Chan <elainechan@users.noreply.github.com>
2016-08-25 14:40:19 -0400

    Create linked_list.py

---
+++ linked_list.py
@@ -0,0 +1,38 @@
+'''
+linked_list.py
+'''
+
+class Node:
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.list = []
+        self.append()
+        self.next_node = next_node
+
+    def append(self):
+        self.list.append(self.data)
+
+    def new_head(self):
+    	self.data = data
+
+class LinkedList(list):
+	
+    def __init__(self, next_node):
+        self.next_node = next_node
+
+    def append(self, item):
+        self.head = Node(item, next_node)
+
+    def __len__(self):
+        self.size = len()
+
+    def __getitem__(self):
+        pass
+
+#    def pop(self):
+#        pass
+#
+#    def __delitem__(self):
+#        pass
+
